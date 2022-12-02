889ac7b2ad2df14b776b4bf26d9a26178ce44a9c
AndreyNesterov <nest.ande@yandex.ru>
2022-12-03 02:49:03 +0300

    add task_04

--- task_04.py
+++ task_04.py
@@ -3,3 +3,19 @@
 # *Пример:*
 # - для k = 8 список будет выглядеть так:
 # [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+num = int(input('Enter num: '))
+fib = [0,1]
+for i in range(2, num + 1):
+    fib.append(fib[i - 1] + fib[i - 2])
+
+fib.insert(0, 1)
+for i in range(1,num):
+    fib.insert(0,fib[1] - fib[0])
+print(fib)
+
+
+
+
+
+
+
